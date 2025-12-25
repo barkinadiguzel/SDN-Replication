3248451711652debb8140292fbca3b8af4e8d30d
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-25 15:57:20 +0300

    Update conv_block.py

--- src/layers/conv_block.py
+++ src/layers/conv_block.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 
 class ConvBlock(nn.Module):
-    """A basic convolutional block: Conv2d -> Normalization -> Activation"""
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, norm='batch', act='relu'):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
